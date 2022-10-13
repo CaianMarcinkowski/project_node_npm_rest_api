f91d3960fce40ff4f7ffda2faa3cc54a9b9611c7
Caian marcinkowski <caianferre@gmail.com>
2022-10-13 17:44:19 -0300

    Adicionando end point de buscar os pokemons da pokeapi

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const request = require("request");
+
 const express = require("express");
 const app = express();
 
 app.listen(3001);
+
+app.get("/", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "http://pokeapi.co/api/v2/pokemon/",
+    headers: { Accept: "application/json" },
+  };
+
+  request(options, function (err, response) {
+    if (err) throw new Error(err);
+    res.send(response.body);
+  });
+});
 
 app.use(express.json());
